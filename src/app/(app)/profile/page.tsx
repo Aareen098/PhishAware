bb60f55e7009bcdb9da1243f806cd9770520247e
Aareen <anandaareen@gmail.com>
2025-10-26 19:07:31 +0000

    Running build in Washington, D.C., USA (East) – iad1 Build machine confi

--- src/app/(app)/profile/page.tsx
+++ src/app/(app)/profile/page.tsx
@@ -6,14 +6,15 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { useUser } from "@/firebase";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Mail, Users } from "lucide-react";
 
 export default function ProfilePage() {
     const { user, isUserLoading } = useUser();
     const fallback = user?.displayName?.substring(0, 2).toUpperCase() || user?.email?.substring(0, 2).toUpperCase() || "PA";
 
     return (
-        <div className="mx-auto w-full max-w-2xl">
-            <div className="mb-8 flex flex-col items-center gap-4">
+        <div className="mx-auto w-full max-w-2xl space-y-8">
+            <div className="flex flex-col items-center gap-4">
                  {isUserLoading ? (
                     <>
                         <Skeleton className="h-24 w-24 rounded-full" />
@@ -35,6 +36,24 @@ export default function ProfilePage() {
                     </>
                 )}
             </div>
+
+            {user && !isUserLoading && (
+                <Card>
+                    <CardContent className="pt-6 space-y-4">
+                        <div className="w-full space-y-2">
+                            <div className="flex items-center text-sm">
+                                <Users className="mr-2 h-4 w-4 text-muted-foreground" />
+                                <span className="text-muted-foreground">Joined on {new Date(user.metadata.creationTime!).toLocaleDateString()}</span>
+                            </div>
+                            <div className="flex items-center text-sm">
+                                <Mail className="mr-2 h-4 w-4 text-muted-foreground" />
+                                <span className="text-muted-foreground">{user.email}</span>
+                            </div>
+                        </div>
+                    </CardContent>
+                </Card>
+            )}
+
             <Card>
                 <CardHeader>
                     <CardTitle>Edit Profile</CardTitle>
