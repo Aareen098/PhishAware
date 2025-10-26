454a6efa7da34bdf36864f79d65557a890222a85
Aareen <anandaareen@gmail.com>
2025-10-26 19:39:52 +0000

    yo

--- src/app/api/genkit/[...slug]/route.ts
+++ src/app/api/genkit/[...slug]/route.ts
@@ -1,9 +1,10 @@
+
 /**
  * @fileoverview This is the entrypoint for Genkit's Next.js plugin.
  *
  * This file should not be modified.
  */
-import { nextJSHandler } from 'genkit-nextjs-plugin';
+import { nextJSHandler } from '@genkit-ai/nextjs';
 import '@/ai/flows/quiz-feedback-flow';
 
 export const POST = nextJSHandler;
